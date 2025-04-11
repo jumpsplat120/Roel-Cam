191fd0e81458649958c57f3ea07c1ef3e3d7d7bd
jumpsplat120 <jumpsplat120@yahoo.com>
2025-04-11 01:16:54 -0700

    COMMENTS

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ import os
 import copy
 import random
 
+#NOTE: CTRL+F for NOTE when looking through code for all the gotcha's and quirks.
+#NOTE: Make sure you create a DISCORD_KEY file where you put your discord key.
+
 def streamToURL(url, quality = "best"):
     streams = streamlink.streams(url)
     
@@ -288,6 +291,17 @@ async def main():
             if frame is None:
                 return
 
+            #The detection code was pulled pretty much wholesale from the
+            #following repo:
+            #https://github.com/itberrios/CV_projects/blob/main/motion_detection/detection_with_background_subtraction.ipynb
+            #There's a corresponding Medium article, but TLDR
+            #Get a bunch of frames to determine the "background", then
+            #anything on the background is now "object". Get bounding
+            #boxes of object. Motion detected. Fine tuning can be done
+            #to account for shadows and the like, but I've left it mostly
+            #default, except to increase the bounding box threshold
+            #so that it doesn't find 10 pixels and assume that the noise
+            #is motion.
             detections = getDetections(self.bgsub, 
                                 cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), 
                                 bbox_thresh = 10000, 
@@ -295,7 +309,7 @@ async def main():
                                 kernel = self.kernel)
             
             #If there's been a dramatic change in the scene, then set detected_motion
-            #to gif_length. MAGIC NUMBER
+            #to gif_length. NOTE: MAGIC NUMBER
             if len(detections) > 2:
                 print(f"Motion! Things detected: {len(detections)}")
             
@@ -308,21 +322,31 @@ async def main():
 
                 self.recording = True
             
-            #If the detected_motion counter is above 0, countdown.
+            #If the detected_motion counter is above 0, countdown. When we hit
+            #zero, we're done recording. This value can continuously get reset
+            #if there continues to be motion, which is why we don't just check
+            #the framecount; a video can be anywhere from 10 - 30 seconds.
             if self.detected_motion > 0:
                 self.detected_motion = self.detected_motion - 1
 
-            #If we're recording, add the color correct frame to the frames array.
+            #If we're recording, add the color corrected frame to the frames array.
             if self.recording:
                 self.frames.append(drawBoundBoxes(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), detections))
             
             #Max amount of frames a vid can have, to avoid vids being too big.
+            #Discord limit.
             if len(self.frames) > self.gif_max:
                 self.detected_motion = 0
 
-            #If detected_motion counted all the way down, then we've
-            #finished grabbing all interesting frames.
+            #If detected_motion has counted all the way down, then we've
+            #finished grabbing all interesting frames. Run the createVideo
+            #method without waiting for it to finish, because there *might*
+            #be more stuff we want to capture. Also, we copy the frames
+            #so that newly added frames don't fuck with the list while we're
+            #converting to video.
             if self.detected_motion == 0 and self.recording:
+                #This loop is the discord.py event loop. There may be another
+                #way to get this via the lib itself, but eh.
                 loop = asyncio.get_running_loop()
                 
                 loop.create_task(self.createVideo(copy.deepcopy(self.frames)))
@@ -331,8 +355,15 @@ async def main():
                 self.recording = False
 
         async def createVideo(self, frames):
+            #Create a randomly named file. We don't use TemporaryNamedFile
+            #to avoid permission issues (possibly just a windows thing, could
+            #probably be fixed by running the Terminal in Admin mode but w/e)
             filename = str(uuid.uuid4()) + ".mp4"
 
+            #spawn a seperate thread when doing imageIO conversion to an mp4
+            #by default it's blocking, which then causes the Discord bot to
+            #freak out cause it gets backed up. So a seperate thread lets
+            #it process without that being a problem. 
             thread = Thread(
                 target = imageio.v3.imwrite,
                 args = (filename, frames),
@@ -344,10 +375,7 @@ async def main():
             thread.start()
             
             #Wait until thread is finished
-            while True:
-                if thread.is_alive():
-                    break
-                
+            while thread.is_alive():                
                 await asyncio.sleep(0)
 
             #Arbitrarily wait an amount of time so that the file is
@@ -358,6 +386,11 @@ async def main():
             await asyncio.sleep(10)
             await self.sendVideo(filename)
             
+            #NOTE: For reasons unknown, this only works sometimes? Like,
+            #most videos get removed, but not all of them. Probably another
+            #"you can access this but not really" issue, where python has it
+            #for discord.File, and then sends it, and then needs to release it
+            #but it hasn't yet, so we can't os.remove. Idk.
             os.remove(filename)
 
         async def sendVideo(self, path):
@@ -371,6 +404,8 @@ async def main():
 
                 print("No video channel is set!")
             else:
+                #Send the channel to a single channel, and forward everywhere else
+                #cause we're being nice to the corporation's bandwidth
                 message = await self.bot.video_channel.send(random.choice(self.cat_lines), file = file)
                 message_ref = discord.MessageReference.from_message(message)
 
